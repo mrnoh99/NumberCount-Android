8ead1f06f98a3142faef428e76797f53b3d3cfd9
mrnoh99 <34833225+mrnoh99@users.noreply.github.com>
2026-05-07 12:42:42 +0900

    Initial Commit

---
+++ NumberCount/NumberCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCountApp.swift
+//  NumberCount
+//
+//  Created by NohJaisung on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct NumberCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
